d6a8ed901e28aea802dce7bbf78787e8421a57bc
OnMyTeam <sangiki07@gmail.com>
2021-04-26 06:43:21 +0000

    chore: 디렉토리 구조 변경 및 파일명 변경 sendBWTransaction.go -> sender/sender.go

---
+++ BWAggregator/sender/sender.go
@@ -0,0 +1,135 @@
+/*
+Copyright 2020 IBM All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"reflect"
+
+	"github.com/hyperledger/fabric-sdk-go/pkg/core/config"
+	"github.com/hyperledger/fabric-sdk-go/pkg/gateway"
+)
+
+func main() {
+	log.Println("============ application-golang starts ============")
+
+	err := os.Setenv("DISCOVERY_AS_LOCALHOST", "true")
+	if err != nil {
+		log.Fatalf("Error setting DISCOVERY_AS_LOCALHOST environemnt variable: %v", err)
+	}
+
+	wallet, err := gateway.NewFileSystemWallet("wallet")
+	if err != nil {
+		log.Fatalf("Failed to create wallet: %v", err)
+	}
+
+	if !wallet.Exists("appUser") {
+		err = populateWallet(wallet)
+		if err != nil {
+			log.Fatalf("Failed to populate wallet contents: %v", err)
+		}
+	}
+
+	ccpPath := filepath.Join(
+		"..",
+		"..",
+		"test-network",
+		"organizations",
+		"peerOrganizations",
+		"org1.example.com",
+		"connection-org1.yaml",
+	)
+
+	gw, err := gateway.Connect(
+		gateway.WithConfig(config.FromFile(filepath.Clean(ccpPath))),
+		gateway.WithIdentity(wallet, "appUser"),
+	)
+	if err != nil {
+		log.Fatalf("Failed to connect to gateway: %v", err)
+	}
+	defer gw.Close()
+
+	network, err := gw.GetNetwork("mychannel")
+	if err != nil {
+		log.Fatalf("Failed to get network: %v", err)
+	}
+
+	contract := network.GetContract("fabcar")
+	readChaincode(contract)
+
+	log.Println("============ application-golang ends ============")
+}
+func readChaincode(contract *gateway.Contract) ([]uint8, error) {
+	// var dat map[string]interface{}
+	result, err := contract.EvaluateTransaction("QueryCar", "CAR0")
+	fmt.Println(result)
+	if err != nil {
+		log.Fatalf("Failed to evaluate transaction: %v", err)
+	}
+	log.Println("init ->", result)
+	log.Println("Type ->", reflect.TypeOf(result))
+	log.Println("string ->", string(result))
+	var objmap map[string]interface{}
+	err1 := json.Unmarshal(result, &objmap)
+	fmt.Println("objmap, err1: ", objmap["amount"], err1)
+	return result, nil
+}
+func writeChaincode(contract *gateway.Contract) error {
+	result, err := readChaincode(contract)
+
+	result, err = contract.SubmitTransaction("BuyCar", "CAR0", string(result))
+	if err != nil {
+		log.Fatalf("failed to evaluate transaction: %v", err)
+	}
+	log.Println(string(result))
+	return nil
+}
+func populateWallet(wallet *gateway.Wallet) error {
+	log.Println("============ Populating wallet ============")
+	credPath := filepath.Join(
+		"..",
+		"..",
+		"test-network",
+		"organizations",
+		"peerOrganizations",
+		"org1.example.com",
+		"users",
+		"User1@org1.example.com",
+		"msp",
+	)
+
+	certPath := filepath.Join(credPath, "signcerts", "cert.pem")
+	// read the certificate pem
+	cert, err := ioutil.ReadFile(filepath.Clean(certPath))
+	if err != nil {
+		return err
+	}
+
+	keyDir := filepath.Join(credPath, "keystore")
+	// there's a single file in this dir containing the private key
+	files, err := ioutil.ReadDir(keyDir)
+	if err != nil {
+		return err
+	}
+	if len(files) != 1 {
+		return fmt.Errorf("keystore folder should have contain one file")
+	}
+	keyPath := filepath.Join(keyDir, files[0].Name())
+	key, err := ioutil.ReadFile(filepath.Clean(keyPath))
+	if err != nil {
+		return err
+	}
+
+	identity := gateway.NewX509Identity("Org1MSP", string(cert), string(key))
+
+	return wallet.Put("appUser", identity)
+}
