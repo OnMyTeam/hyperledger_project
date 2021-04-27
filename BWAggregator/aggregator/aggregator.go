f05e9667adfdbdb4b42620f75b5f5c32069831b8
OnMyTeam <sangiki07@gmail.com>
2021-04-27 06:13:35 +0000

    Add function(not implementation) MakeBWTxset, Aggregate, MakeWriteValue, SendTxProposal

--- BWAggregator/aggregator/aggregator.go
+++ BWAggregator/aggregator/aggregator.go
@@ -1,22 +1,60 @@
 package aggregator
 
 import (
+	"fmt"
 	protos "hyperledger_project/BWAggregator/protos"
+	"sync"
+	"time"
 )
 
 type Aggregator struct {
 	BWTxChan chan *protos.BWTransaction
+	BWTxSet  []*protos.BWTransaction
 }
 
 func Init() *Aggregator {
 	aggregator := &Aggregator{
 
 		BWTxChan: make(chan *protos.BWTransaction, 5),
+		BWTxSet:  make([]*protos.BWTransaction, 5),
 	}
+	go aggregator.MakeBWTxset()
 	return aggregator
 
 }
+func (aggregator *Aggregator) MakeBWTxset() {
+	var mutex = &sync.Mutex{}
+	count := 0
+	// timer := time.NewTimer(time.Second * 3)
+	ticker := time.NewTicker(time.Millisecond * 1500)
+	for {
+		select {
+		case BWTx := <-aggregator.GetBWTxReceiveChannel():
+			fmt.Println(BWTx)
+			aggregator.BWTxSet = append(aggregator.BWTxSet, BWTx)
+			count++
+		case <-ticker.C:
+			mutex.Lock()
+			for i, s := range aggregator.BWTxSet {
+				fmt.Println(i, s)
+			}
+			fmt.Println("=================")
+			mutex.Unlock()
+			ticker = time.NewTicker(time.Millisecond * 1500)
+		}
 
+	}
+
+}
+func (aggregator *Aggregator) Aggregate() {
+
+}
+func (aggregator *Aggregator) MakeWriteValue() {
+
+}
+func (aggregator *Aggregator) SendTxProposals() {
+
+}
 func (aggregator *Aggregator) GetBWTxReceiveChannel() <-chan *protos.BWTransaction {
 	return aggregator.BWTxChan
 }
--- BWAggregator/aggregator/operator_type.go
+++ BWAggregator/aggregator/operator_type.go
@@ -1,3 +1,4 @@
+package aggregator
 
 type OperatorType int
 
