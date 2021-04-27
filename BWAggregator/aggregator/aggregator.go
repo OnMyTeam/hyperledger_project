0330404c1b5833cbcbf2f91a9bdb53765c1e0f0e
OnMyTeam <sangiki07@gmail.com>
2021-04-27 14:28:33 +0000

    FEAT: Receive BWTxset in Aggregate Function

--- BWAggregator/aggregator/aggregator.go
+++ BWAggregator/aggregator/aggregator.go
@@ -8,46 +8,62 @@ import (
 )
 
 type Aggregator struct {
-	BWTxChan chan *protos.BWTransaction
-	BWTxSet  []*protos.BWTransaction
+	BWTxChan    chan *protos.BWTransaction
+	BWTxSetChan chan []*protos.BWTransaction
+	BWTxSet     []*protos.BWTransaction
 }
 
 func Init() *Aggregator {
 	aggregator := &Aggregator{
 
-		BWTxChan: make(chan *protos.BWTransaction, 5),
-		BWTxSet:  make([]*protos.BWTransaction, 5),
+		BWTxChan:    make(chan *protos.BWTransaction, 5),
+		BWTxSetChan: make(chan []*protos.BWTransaction, 5),
+		BWTxSet:     make([]*protos.BWTransaction, 5),
 	}
 	go aggregator.MakeBWTxset()
+	go aggregator.Aggregate()
 	return aggregator
 
 }
 func (aggregator *Aggregator) MakeBWTxset() {
 	var mutex = &sync.Mutex{}
-	count := 0
-	// timer := time.NewTimer(time.Second * 3)
+
 	ticker := time.NewTicker(time.Millisecond * 1500)
 	for {
 		select {
 		case BWTx := <-aggregator.GetBWTxReceiveChannel():
+			fmt.Println("=========== MakeBWTxset ===========")
 			fmt.Println(BWTx)
 			aggregator.BWTxSet = append(aggregator.BWTxSet, BWTx)
-			count++
+
 		case <-ticker.C:
+			copyBWTxset := make([]*protos.BWTransaction, len(aggregator.BWTxSet))
 			mutex.Lock()
-			for i, s := range aggregator.BWTxSet {
-				fmt.Println(i, s)
-			}
-			fmt.Println("=================")
+			copy(copyBWTxset, aggregator.BWTxSet)
+			aggregator.BWTxSet = make([]*protos.BWTransaction, 5)
+			aggregator.GetBWTxSetSendChannel() <- copyBWTxset
 			mutex.Unlock()
 			ticker = time.NewTicker(time.Millisecond * 1500)
 		}
+		fmt.Println("=========== MakeBWTxsetEnd ===========")
 
 	}
 
 }
-func (aggregator *Aggregator) Aggregate() {
 
+// Aggregate BWTxset을 활용하여 연산 후 각 키에 Write할 value생성
+func (aggregator *Aggregator) Aggregate() {
+	for {
+		select {
+		case BWTxset := <-aggregator.GetBWTxSetReceiveChannel():
+			fmt.Println("=========== Aggregator ===========")
+			for i, s := range BWTxset {
+				fmt.Println(i, s)
+			}
+		}
+		fmt.Println("=========== AggregatorEnd ===========")
+
+	}
 }
 func (aggregator *Aggregator) MakeWriteValue() {
 
@@ -60,4 +76,10 @@ func (aggregator *Aggregator) GetBWTxReceiveChannel() <-chan *protos.BWTransacti
 }
 func (aggregator *Aggregator) GetBWTxSendChannel() chan<- *protos.BWTransaction {
 	return aggregator.BWTxChan
+}
+func (aggregator *Aggregator) GetBWTxSetReceiveChannel() <-chan []*protos.BWTransaction {
+	return aggregator.BWTxSetChan
+}
+func (aggregator *Aggregator) GetBWTxSetSendChannel() chan<- []*protos.BWTransaction {
+	return aggregator.BWTxSetChan
 }
