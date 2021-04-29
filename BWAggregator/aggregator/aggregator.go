8a2668a06502289791d51b282df22a2cecaf9c02
OnMyTeam <sangiki07@gmail.com>
2021-04-29 05:03:22 +0000

    기능추가: 각 키별 연산후 Write Value 생성

--- BWAggregator/aggregator/aggregator.go
+++ BWAggregator/aggregator/aggregator.go
@@ -8,17 +8,23 @@ import (
 )
 
 type Aggregator struct {
-	BWTxChan    chan *protos.BWTransaction
-	BWTxSetChan chan []*protos.BWTransaction
-	BWTxSet     []*protos.BWTransaction
+	BWTxChan        chan *protos.BWTransaction
+	BWTxSetChan     chan []*protos.BWTransaction
+	BWTxSet         []*protos.BWTransaction
+	AggregateResult map[string]Result
+}
+type Result struct {
+	FunctionName string
+	Key          string
+	WriteValue   int
 }
 
 func Init() *Aggregator {
 	aggregator := &Aggregator{
 
-		BWTxChan:    make(chan *protos.BWTransaction, 5),
-		BWTxSetChan: make(chan []*protos.BWTransaction, 5),
-		BWTxSet:     make([]*protos.BWTransaction, 5),
+		BWTxChan:        make(chan *protos.BWTransaction),
+		BWTxSetChan:     make(chan []*protos.BWTransaction),
+		AggregateResult: make(map[string]Result),
 	}
 	go aggregator.MakeBWTxset()
 	go aggregator.Aggregate()
@@ -32,20 +38,22 @@ func (aggregator *Aggregator) MakeBWTxset() {
 	for {
 		select {
 		case BWTx := <-aggregator.GetBWTxReceiveChannel():
-			fmt.Println("=========== MakeBWTxset ===========")
+			fmt.Println("=========== ReceiveBWTxset ===========")
 			fmt.Println(BWTx)
 			aggregator.BWTxSet = append(aggregator.BWTxSet, BWTx)
+			fmt.Println("=========== EndReceiveBWTxset ===========")
 
 		case <-ticker.C:
+			fmt.Println("=========== MakeBWTxset ===========")
 			copyBWTxset := make([]*protos.BWTransaction, len(aggregator.BWTxSet))
 			mutex.Lock()
 			copy(copyBWTxset, aggregator.BWTxSet)
-			aggregator.BWTxSet = make([]*protos.BWTransaction, 5)
+			aggregator.BWTxSet = nil
 			aggregator.GetBWTxSetSendChannel() <- copyBWTxset
 			mutex.Unlock()
 			ticker = time.NewTicker(time.Millisecond * 1500)
+			fmt.Println("=========== EndMakeBWTxset ===========")
 		}
-		fmt.Println("=========== MakeBWTxsetEnd ===========")
 
 	}
 
@@ -56,12 +64,36 @@ func (aggregator *Aggregator) Aggregate() {
 	for {
 		select {
 		case BWTxset := <-aggregator.GetBWTxSetReceiveChannel():
-			fmt.Println("=========== Aggregator ===========")
-			for i, s := range BWTxset {
-				fmt.Println(i, s)
+			fmt.Println("=========== Aggregate ===========")
+			for _, BWTx := range BWTxset {
+				key := BWTx.Key
+				result := aggregator.AggregateResult[key]
+
+				//empty struct check
+				if result.FunctionName == "" {
+					result.FunctionName = BWTx.Functionname
+					result.Key = BWTx.Key
+					result.WriteValue = int(BWTx.Operand)
+					aggregator.AggregateResult[key] = result
+				} else {
+					// 사전 사후 검사
+					if result.WriteValue < int(BWTx.Precondition) && result.WriteValue > int(BWTx.Postcondition) {
+						fmt.Println("no!!!!!!!!")
+					} else {
+						if BWTx.Operator == int32(ADD) {
+							result.WriteValue += int(BWTx.Operand)
+						}
+						aggregator.AggregateResult[key] = result
+					}
+				}
+				fmt.Println(result.FunctionName)
+				fmt.Println(result.Key)
+				fmt.Println(result.WriteValue)
+
 			}
+			fmt.Println(aggregator.AggregateResult)
+			fmt.Println("=========== EndAggregate ===========")
 		}
-		fmt.Println("=========== AggregatorEnd ===========")
 
 	}
 }
