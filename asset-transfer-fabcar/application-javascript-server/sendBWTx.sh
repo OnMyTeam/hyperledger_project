a37333065c19af99cad1526abe25ea5ad03332d4
OnMyTeam <sangiki07@gmail.com>
2021-05-02 09:52:20 +0000

    신규: 랜덤하게 BwTx전송을 위한 스크립트 파일 생성

---
+++ asset-transfer-fabcar/application-javascript-server/sendBWTx.sh
@@ -0,0 +1,20 @@
+
+
+echo $random_num
+
+SET=$(seq 0 100)
+
+for i in $SET
+
+do
+
+    echo "Running loop seq "$i
+	random_num=$(cat /dev/urandom | tr -dc '0-2' | fold -w 1 | sed 1q)
+	tx_num=$(cat /dev/urandom | tr -dc '0-10' | fold -w 1 | sed 1q)
+	node index.js $tx_num
+	sleep $random_num
+    # some instructions
+
+done
+
+
